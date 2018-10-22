de5404e0006852f177acdbc9e62a3482dc53acde
WANG Wei <18438350@cs12183.comp.hkbu.edu.hk>
2018-10-22 18:54:38 +0800

    Initial commit

---
+++ api/models/Person.js
@@ -0,0 +1,48 @@
+/**
+ * Person.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    name: {
+      type: "string"
+    },
+
+    age: {
+      type: "number"
+    },
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+  getInvalidIdMsg: function (opts) {
+      
+    // opts = req.params
+
+    if (typeof opts.id === "undefined" )
+      return "Person id not specified or with incorrect type.";
+
+      if(isNaN(parseInt(opts.id)))
+      return "person id with incorrect type "
+
+    return null;        // falsy
+
+  },
+
+};
+
